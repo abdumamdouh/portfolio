881eac6c4d62a210420d26df8022fb838279929d
abdumamdouh <eabdogfx@gmail.com>
2022-02-04 20:52:30 +0200

    contact me info

--- src/portfolio.js
+++ src/portfolio.js
@@ -323,8 +323,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+02-01156720635",
+  email_address: "eabdogfx@gmail.com"
 };
 
 // Twitter Section
