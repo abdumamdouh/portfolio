20748e1e1c852d951e607d8e09eec7addbe9f7e9
abdumamdouh <eabdogfx@gmail.com>
2022-02-04 20:47:26 +0200

    add Achievements

--- src/portfolio.js
+++ src/portfolio.js
@@ -244,22 +244,18 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "ICPC - International Collegiate Programming Contest",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
+        "ECPC 2021, Rank: 5, Fifth Place.",
+      image: require("./assets/images/acmLogo.png"),
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+          url: "https://drive.google.com/file/d/1VTW88IvKQpVnSSVlWX6NTQrSAN4ebazs/view?usp=sharing"
         },
         {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          name: "ICPC",
+          url: "https://icpc.global/ICPCID/KLQTA9XXHQMB"
         }
       ]
     }
